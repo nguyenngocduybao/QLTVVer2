eeb00e3fc2748ceb325eaccf53dd816f1bed683e
DESKTOP-MUK2RPJ\Chelsea198 <phantrongba1998@gmail.com>
2018-07-07 14:38:15 +0700

    Xử lý các hàm trong class TheDocGiaService và ITheDocGiaService

---
+++ FirstVer29062018/QLTV/Service/IService/ITheDocGiaService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Dtos;
+using Data.DTO;
+namespace Service.IService
+{
+    public interface ITheDocGiaService
+    {
+        // Add From"TheDocGia" and "User"
+        void AddTheDocGiaAndUser(TheDocGiaDtos DocGia, UserDtos user);
+        #region getListSearch From "TheDocGia"
+        List<TheDocGiaDTO> getFromTheDocGiaSearchHoTen(string TenDG);
+        List<TheDocGiaDTO> getFromTheDocGiaSearchEmaiLDG(string EmailDG);
+        List<TheDocGiaDTO> getFormTheDocGiaSearchDiaChiDG(string DiaChiDG);
+        List<TheDocGiaDTO> getFormTheDocGiaSearchNgayLapThe(DateTime Ngaylap);
+        List<TheDocGiaDTO> getFormTheDocGiaSearchNgaySinhDG(DateTime NgaySinh);
+        #endregion
+    }
+}
