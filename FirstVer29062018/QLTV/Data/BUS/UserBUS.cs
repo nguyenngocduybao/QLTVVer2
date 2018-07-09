8ba74426d63e26dc713e247c03e3e701383d5e1c
DESKTOP-MUK2RPJ\Chelsea198 <phantrongba1998@gmail.com>
2018-07-09 23:11:28 +0700

    Tạo các hàm tìm kiếm theo thuộc tính trong class UserDAO , LoaiSachDAO,USerBUS(IUserBUS )(Bổ sung hàm thêm) và LoaiSachBUS(ILoaiSachBUS) Xử lý các hàm tương ứng từ iBUS xuống tầng Service

--- FirstVer29062018/QLTV/Data/BUS/UserBUS.cs
+++ FirstVer29062018/QLTV/Data/BUS/UserBUS.cs
@@ -12,8 +12,9 @@ namespace Data.BUS
 {
     public class UserBUS : IUserBUS
     {
+
         #region getListSearch UserDTO
-        // search UserName
+        //getListSearch UserName From "User"
         public List<UserDTO> getFromUserSearchUserName(string Name)
         {
             try
@@ -28,6 +29,14 @@ namespace Data.BUS
 
                 throw;
             }
+        }
+        // getListSearch IDDocGia From "User"
+        public List<UserDTO> getFromUserSearchIDDocGia(int IDDG)
+        {
+            List<UserDTO> listUserDTO = new List<UserDTO>();
+            listUserDTO = UserDAO.Instance.getFromUserSearchIDDocGia(IDDG);
+            return listUserDTO;
+
         }
         #endregion
     }
