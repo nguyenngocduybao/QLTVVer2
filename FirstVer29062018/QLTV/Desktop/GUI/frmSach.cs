1f3b02268ead2996793d9bd8d9c47e1f8d1e29ff
benbacker <40685914+benbacker@users.noreply.github.com>
2018-07-31 16:03:08 +0700

    desgin frm sách + chỉnh sữa frm Main

---
+++ FirstVer29062018/QLTV/Desktop/GUI/frmSach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop.GUI
+{
+    public partial class frmSach : Form
+    {
+        public frmSach()
+        {
+            InitializeComponent();
+        }
+
+        #region Value
+        public string TenDauSach;
+        public string NhaXB;
+        public int NamXB;
+        public int SoLuongTon;
+        public decimal GiaTien;
+        public int SoLuong;
+        public decimal DonGia;
+        public decimal TongTien;
+        #endregion
+    }
+}
