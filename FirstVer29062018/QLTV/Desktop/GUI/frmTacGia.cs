2aa3184875d515b67ffaa4f6546b2cd4f6cc856f
benbacker <40685914+benbacker@users.noreply.github.com>
2018-08-10 22:36:00 +0700

    autocomplete tên tác giả

--- FirstVer29062018/QLTV/Desktop/GUI/frmTacGia.cs
+++ FirstVer29062018/QLTV/Desktop/GUI/frmTacGia.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Desktop.HelperUI;
 using Data.Dtos;
 using Service.ABSTRACT;
+using Data.DAO;
 
 namespace Desktop.GUI
 {
@@ -33,6 +34,23 @@ namespace Desktop.GUI
         private void dt_NgaySinh_ValueChanged(object sender, EventArgs e)
         {
             NgaySinh = dt_NgaySinh.Value;
+        }
+        #endregion
+        #region AutoComplete
+        public void AutoCompleteTenTG()
+        {
+            string[] TenTacGia = new string[10000];
+            TenTacGia =  GetDataDAO.Instance.getArrayTenTacGia();
+            AutoCompleteStringCollection data = new AutoCompleteStringCollection();
+            data.AddRange(TenTacGia.ToArray());
+            tb_TenTacGia.AutoCompleteCustomSource = data;
+            tb_TenTacGia.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+        #endregion
+        #region LoadfrmTacGia
+        private void frmTacGia_Load(object sender, EventArgs e)
+        {
+            AutoCompleteTenTG();
         }
         #endregion
     }
